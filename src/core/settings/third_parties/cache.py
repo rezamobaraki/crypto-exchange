630f2c9becc6939a6aa257e6de4ce98f58904925
Reza Mobaraki <rezam578@gmail.com>
2024-09-25 23:01:28 +0330

    fix(): cache timeout

--- src/core/settings/third_parties/cache.py
+++ src/core/settings/third_parties/cache.py
@@ -16,4 +16,10 @@ CACHES = {
 # Cache time to live is 15 minutes by default.
 CACHE_TTL = env.int("CACHE_TTL_MINUTES", 15) * 60
 
-CACHE_TIMEOUT = env.int("CACHE_TIMEOUT", 60) * 60
+DEFAULT_CACHE_TIMEOUT = 60 * 60  # 1 hour in seconds
+VOLATILE_CACHE_TIMEOUT = 5 * 60  # 5 minutes in seconds
+
+CACHE_TIMEOUTS = {
+    'default': env.int("DEFAULT_CACHE_TIMEOUT", DEFAULT_CACHE_TIMEOUT),
+    'volatile': env.int("VOLATILE_CACHE_TIMEOUT", VOLATILE_CACHE_TIMEOUT),
+}
