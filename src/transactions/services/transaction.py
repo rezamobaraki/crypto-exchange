f5848a172dc28dbb8370a5e2a8af2c90d0128843
Reza Mobaraki <rezam578@gmail.com>
2024-10-09 11:43:02 +0330

    fix(migrations): add content type to transaction

--- src/transactions/services/transaction.py
+++ src/transactions/services/transaction.py
@@ -1,18 +1,19 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction
 
+from accounts.models import Wallet
 from transactions.enums import TransactionTypes
 from transactions.models.transaction import Transaction
 
 
 class TransactionService:
-    model = Transaction
+    repository = Transaction
 
     @classmethod
     @transaction.atomic
-    def wallet_withdraw(cls, *, wallet_id, amount, related_object) -> int:
+    def wallet_withdraw(cls, *, wallet_id, amount, related_object):
         content_type = ContentType.objects.get_for_model(related_object)
-        return cls.model.objects.create(
+        return cls.repository.objects.create(
             wallet_id=wallet_id,
             amount=amount,
             content_type=content_type,
@@ -22,5 +23,12 @@ class TransactionService:
 
     @classmethod
     @transaction.atomic
-    def wallet_deposit(cls, *, wallet_id, amount) -> int:
-        return cls.model.objects.create(wallet_id=wallet_id, amount=amount, type=TransactionTypes.DEPOSIT)
+    def wallet_deposit(cls, *, wallet_id, amount):
+        content_type = ContentType.objects.get_for_model(Wallet)
+        return cls.repository.objects.create(
+            wallet_id=wallet_id,
+            amount=amount,
+            content_type=content_type,
+            object_id=wallet_id,
+            type=TransactionTypes.DEPOSIT
+        )
