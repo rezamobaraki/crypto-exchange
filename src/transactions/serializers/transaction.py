d9362e29cd41fa7b555346ab281d03e7da31649e
Reza Mobaraki <rezam578@gmail.com>
2024-10-09 11:59:53 +0330

    fix(transactio): fix serializer

--- src/transactions/serializers/transaction.py
+++ src/transactions/serializers/transaction.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+
 from transactions.models.transaction import Transaction
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    content_type_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Transaction
-        fields = ("id", "wallet", "amount", "content_type", "object_id", "type")
+        fields = ("id", "wallet", "amount", "content_type", "content_type_name", "object_id", "type")
         read_only_fields = fields
+
+    def get_content_type_name(self, obj):
+        return obj.content_type.model
